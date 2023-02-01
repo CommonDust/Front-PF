c447efa2138f0ce8469bd687c503e1dd22027a87
ulises <ulisessahlin@hotmail.com>
2023-01-31 23:50:50 -0300

    Haciendo inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 
 const Form = () => {
+
+
+    handleChange = (e) => {
+      e.target.name
+    }
+
   return (
     <div>
-        <input>Name</input>
-        <input>Lastname</input>
-        <input>Age</input>
-
-      <>The Form</>
+      <div>
+      <input type='text' name='name' placeholder='Enter name' />  
+      </div>
+      <div>
+      <input type='text' name='lastname' placeholder='Enter lastname' /> 
+      </div>
+      <div>
+      <input type='text' name='birthday' placeholder='Enter birthday'/> 
+      </div>
+      <div>
+      <input type='text' name='username' placeholder='Enter username'/> 
+      </div>
+      <div>
+      <input type='text' name='hotmail' placeholder='enter hotmail'/> 
+      </div>
+      <div>
+      <input type='text' name='password' placeholder='Enter password' onChange={}/> 
+      </div>
     </div>
   )
 }
