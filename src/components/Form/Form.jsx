f1e109a7a4d538e600a8b7582c76ba265014154b
ulises <ulisessahlin@hotmail.com>
2023-01-31 22:59:01 -0300

    ActualizacionRRDv5.0.0 y Componente Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <input>Name</input>
+        <input>Lastname</input>
+        <input>Age</input>
+
+      <>The Form</>
+    </div>
+  )
+}
+
+export default Form
